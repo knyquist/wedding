58d9b84bb0a84610f301a11a085c9918a3338356
Kristofor Nyquist <me@kristofornyquist.com>
2018-07-10 00:14:43 -0700

    improved navbar interactivity

--- src/app/weddinginfo/weddinginfo.component.ts
+++ src/app/weddinginfo/weddinginfo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-subroute',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeddingInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:ActivatedRoute,private router:Router) { }
 
   ngOnInit() {
+     this.route.fragment.subscribe(f => {
+         const element = document.querySelector('#' + f);
+         if (element) {
+			 const bodyRect = document.body.getBoundingClientRect().top;
+			 const elementRect = element.getBoundingClientRect().top;
+			 const elementPosition = elementRect - bodyRect;
+		     window.scrollTo({top: elementPosition - 75,
+		          			  left: 0,
+		          			  behavior: "smooth"
+		     });
+         }
+         else {
+         	 window.scrollTo({top: 0, 
+         	 				  left: 0, 
+         	 				  behavior: "smooth"});
+         }
+    });
   }
 
 }
